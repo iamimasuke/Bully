f6526d36e7a2192b50c030d03ef1407c347a3d20
iamimasuke <i0103@icloud.com>
2023-12-19 18:35:01 +0900

    もう少し

--- server.py
+++ server.py
@@ -32,7 +32,8 @@ class BullyNode:
     def election(self):
         lock = threading.Lock()
         threads = []
-        self.nodes_replies = []#投票してそのレスポンスを受け取る
+        self.nodes_replies = []
+        self.in_election_in_thread = True
         print(f"Node {self.id}は選挙を開始します")
         print(f'私はNode {self.id}です。今の選挙のタームは{self.election_term}です')
         self.in_election = True
@@ -44,22 +45,30 @@ class BullyNode:
             threads.append(thread)
             print(f'私はNode {self.id}です。Node{higher_node.id}のスレッドを作成しました')
             thread.start()   
+        #hiher_nodes全てからリプライが返ってくるまで待つ
+        #故障は考慮しない
+        time.sleep(0.1)
+        while len(higher_nodes) != len(self.nodes_replies):      
+            time.sleep(0.01)
 
-        if self.id == max([p.id for p in self.processes]) and any(node.in_election for node in self.processes):
-            print(f"私はNode {self.id}です。私が一番大きので退場します")
-            return
-        #処理パート１t1とt2の間に行いたい
-        time.sleep(1)
-        print(f"私はNode {self.id}です。リプライをくれたノードたちは{self.nodes_replies}です")
-        if higher_nodes == self.nodes_replies.sort():  
-            self.in_election = False
-            print(f"Node {self.id}　選挙終了")
+        print(f"私はNode {self.id}です。リプライをくれたノードたちは{self.nodes_replies}です")   
+        self.in_election = False
+        print(f"Node {self.id}　選挙終了")
         if not any(node.in_election for node in self.processes):
             self.become_leader()
+        '''
+        for higher_node in higher_nodes:        
+            proxy = ServerProxy(f"http://localhost:{higher_node.port}")
+            proxy.send_in_election(self.id, self.election_term)
+        ''' 
+    
+    def send_in_election(self, higher_node):
+        return False
  
      #選挙を通知する
     def send_election_to_node(self, higher_node):
         try:
+            print(f"......Node {self.id} はNode {higher_node.id}に選挙を送ります")
             proxy = ServerProxy(f"http://localhost:{higher_node.port}")
             proxy.send_election(self.id, self.election_term)
         except Exception as e:
@@ -69,19 +78,24 @@ class BullyNode:
     #選挙を送る
     def send_election(self, sender_id, sender_election_term):
         #senderにOKを返すスレッドと選挙をするスレッドを作成
+        print(f"ーーーーーNode {self.id}  はNode {sender_id}から色々受け取りました")
         t1 = threading.Thread(target=self.send_node_ok, args=(self.id,sender_id))
         t2 = threading.Thread(target=self.election)
         t1.start()
-        t1.join()
-        #ここのタイミングで処理1を行いたい
+        #t1の処理が終わるまで待つ
+        #senderにOKを返す処理が終わるまで待つため
         time.sleep(2)
+        sender_in_election_in_thread = [p.sender_in_election_in_thread for p in self.processes if p.id == sender_id]
+        while self.in_election_in_thread:
+            time.sleep(0.01)
         self.election_term = sender_election_term + 1
-        print(f"Node {self.id} received election from Node {sender_id}.")
+        print(f"Node {self.id}  はNode {sender_id}から選挙を受け取りました")
         t2.start()
         t2.join()
     
     #node2,3,4が送る
     def send_node_ok(self, self_id,sender_id):
+        self.in_election = True
         sender_port = 0
         for i in self.processes:
             if i.id == sender_id:
@@ -111,7 +125,7 @@ if __name__ == "__main__":
         BullyNode(node_id=1, port=8001, processes=[]),
         BullyNode(node_id=2, port=8002, processes=[]),
         BullyNode(node_id=3, port=8003, processes=[]),
-        BullyNode(node_id=4, port=8004, processes=[]),
+        #BullyNode(node_id=4, port=8004, processes=[]),
     ]
 
     for node in nodes:
