67b0b77402b2c102455b5027e03f0498fbc382bd
iamimasuke <i0103@icloud.com>
2023-12-19 23:58:22 +0900

    完成

--- server.py
+++ server.py
@@ -5,13 +5,17 @@ import time
 
 #class of a node
 class BullyNode:
+
+    election_terms = set()
+
+
     def __init__(self, node_id, port, processes):
         self.id = node_id
         self.port = port
         self.processes = processes
         self.in_election = False
-        self.election_term = 1
         self.nodes_replies = []
+
         #RPCによるサーバーを立てる
         #これによりサーバー間で通信が可能に
         self.server = SimpleXMLRPCServer(("localhost", port), allow_none=True)
@@ -29,13 +33,13 @@ class BullyNode:
         self.server_thread.join()       
 
     #選挙を開始する
-    def election(self):
-        lock = threading.Lock()
+    def election(self,self_term):
+        self.election_terms.add(self_term)
         threads = []
         threads2 = []
         self.nodes_replies = []
         self.in_election_in_thread = True
-        print(f"Node {self.id}は選挙を開始します。今の選挙のタームは{self.election_term}です")
+        print(f"Node {self.id}は選挙を開始します。今の選挙のタームは{self_term}です")
         self.in_election = True
         #自分よりidの大きなノードを探す。あれば選挙するように通達
         higher_nodes = [p for p in self.processes if p.id > self.id]
@@ -53,35 +57,46 @@ class BullyNode:
 
         print(f"私はNode {self.id}です。リプライをくれたノードたちは{self.nodes_replies}です")
         time.sleep(1)   
-        self.in_election = False
-        print(f"Node {self.id}　選挙終了")
-        if not any(node.in_election for node in self.processes):
-            self.become_leader()
-        
         for reply_node in self.nodes_replies:
             reply_nodes = [p for p in self.processes if p.id == reply_node][0]
-            thread2 = threading.Thread(target=reply_nodes.election)
+            thread2 = threading.Thread(target=reply_nodes.election, args=(self_term+1,))
             threads2.append(thread2)
-            print(f'私はNode {self.id}です。Node{higher_node.id}の選挙開始用のスレッドを作成しました')
-            thread2.start()   
+            print(f'私はNode {self.id}です。Node{reply_nodes.id}の選挙開始用のスレッドを作成しました')
+            thread2.start()
+
+
         
+        print(f"Node {self.id}　選挙終了　現在のタームは{self_term}です")
+        if not any(node.in_election for node in self.processes):
+            proxy = ServerProxy(f"http://localhost:{self.port}")
+            proxy.become_leader(self_term)
+        
+        self.in_election = False
+    
+
     #node2,3が実行
     def reply(self,sender):
-        self.election_term = sender.election_term + 1
-        print(f"Node {self.id} の選挙タームは{self.election_term}です")
         self.in_election = True
         sender.nodes_replies.append(self.id)
         print(f"Node {sender.id} はNode {self.id}からOKをもらいました")
 
-    def become_leader(self):
-        print(f"【速報！！！！！】Node {self.id} がリーダーになりました!!.")
-        for node in self.processes:
-            if node.id != self.id:
-                proxy = ServerProxy(f"http://localhost:{node.port}")
-                proxy.receive_leader(self.id)
+    def send_term(self, node):
+        pass
     
+    def become_leader(self,self_term):
+        time.sleep(1)
+        print(f"self_terms: {self.election_terms}")
+        if self_term == max(self.election_terms):
+            print(f"【速報！！！！！】Node {self.id} がリーダーになりました!!.")
+            for node in self.processes:
+                if node.id != self.id:
+                    proxy = ServerProxy(f"http://localhost:{node.port}")
+                    proxy.receive_leader(self.id)
+        else:
+            return
+        
     def receive_leader(self, leader_id):
-        print(f"Node {self.id} received leader from Node {leader_id}.")
+        print(f"Node {self.id} はリーダー通知を{leader_id}から受け取りました")
         self.leader_id = leader_id
         
     
@@ -90,10 +105,13 @@ if __name__ == "__main__":
         BullyNode(node_id=1, port=8001, processes=[]),
         BullyNode(node_id=2, port=8002, processes=[]),
         BullyNode(node_id=3, port=8003, processes=[]),
-        #BullyNode(node_id=4, port=8004, processes=[]),
+        BullyNode(node_id=4, port=8004, processes=[]),
+        BullyNode(node_id=5, port=8005, processes=[]),
+        BullyNode(node_id=6, port=8006, processes=[]),
+
     ]
 
     for node in nodes:
         node.processes = nodes
         
-    nodes[0].election()
+    nodes[0].election(1)
