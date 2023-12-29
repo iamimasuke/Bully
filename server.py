e845073b62ad43f80fb7a6d844980cc667ad1dd5
iamimasuke <i0103@icloud.com>
2023-12-29 16:24:54 +0900

    リプライ消去

--- server.py
+++ server.py
@@ -57,12 +57,18 @@ class BullyNode:
         self.replies = []
         if self.leader_id is None:
             threads = []
+            threads2 = []
             higher_nodes_id = [p for p in BullyNode.processes_id if p > self.id]
             for higher_node_id in higher_nodes_id:
                 t=threading.Thread(target=self.send_election, args=(higher_node_id,))
                 t.daemon = True
                 threads.append(t)
                 t.start()
+                t.join()
+            if self.replies == []:
+                self.become_leader()
+            
+
     
     #それぞれのスレッドがNodeに選挙を通知
     def send_election(self,higher_node_id):
@@ -73,71 +79,34 @@ class BullyNode:
                 #e: cannot marshal recursive dictionaries
                 node_port = 8000 + higher_node_id
                 proxy = ServerProxy(f"http://localhost:{node_port}", allow_none=True)
-                proxy.election(self.id)
-
+                reply = proxy.election(self.id)
+                self.replies.append(reply)
             except Exception as e:
                 print(f"Node {self.id} はNode {higher_node_id} にelectionのRPCを送信できませんでした") 
                 print(f"エラー詳細：{e}")
-            finally:
-                time.sleep(1)
-                self.check_reply()
+            
             
    #選挙を受け取ったNodeが送信元にリプライを送り、自分より大きいNodeに選挙を通知
     def election(self,from_node_id):
-        if self.leader_id is None:
-            #リーダーがいれば終了    
-            if not self.leader_id is None:
-                print(f"Node {self.id} はリーダーがいるので選挙を開始しません")
-                return
-            #リーダーがいなければリプ送って選挙開始
+        #リーダーがいれば終了    
+        if not self.leader_id is None:
+            print(f"Node {self.id} はリーダーがいるので選挙を開始しません")
+            return
+        #リーダーがいなければリプ送って選挙開始
+        else:
+            #リプの送信
+            #自分のidが最大ならリーダーになる
+            if self.id == max(BullyNode.processes_id):
+                self.become_leader()
             else:
-                #リプの送信
-                t1 = threading.Thread(target=self.reply, args=(from_node_id,))
-                t1.daemon = True
-                t1.start()
-                t1.join()
-                #自分のidが最大ならリーダーになる
-                if self.id == max(BullyNode.processes_id):
-                    self.become_leader()
-                else:
-                    t2 = threading.Thread(target=self.send_parallel_election,args=()) 
-                    t2.daemon = True  
-                    t2.start()        
-        
-   #リプライを送信
-    def reply(self,from_node_id):
-        if self.leader_id is None:
-            print(f'Node {self.id} はNode {from_node_id} にリプライを送信しました')
-            #選挙を送ったNodeにリプライを送信
-            try:
-                node_port = 8000 + from_node_id
-                proxy = ServerProxy(f"http://localhost:{node_port}",  allow_none=True)
-                proxy.receive_reply(self.id)
-            except Exception as e:
-                print(f"Node {self.id} はNode {from_node_id} にreceive_replyのRPCを送信できませんでした")
-                print(f"エラー詳細：{e}")
-                return
-            
-    #リプライを受け取る
-    def receive_reply(self,reply_sender_id):
-      #  with BullyNode.lock:
-            if self.leader_id is None:
-                self.replies.append(reply_sender_id)
-                print(f"Node {self.id} はNode {reply_sender_id} からリプライを受け取りました")
-                return
-        
-    def check_reply(self):
-            if self.leader_id is None:
-                if self.replies == []:
-                    self.become_leader()
-                    time.sleep(1)
-                    return
-           
-        
+                t = threading.Thread(target=self.send_parallel_election,args=(),daemon=True)   
+                t.start()        
+        return self.id
+                    
     #リーダーになる
     #他のNodeにregister_leaderを送信
     def become_leader(self):
-        with BullyNode.lock:
+        #with BullyNode.lock:
             if self.leader_id is None:
                 self.leader_id = self.id
                 print(f"【速報！！！！！】Node {self.id} がリーダーになりました!!")
@@ -154,6 +123,8 @@ class BullyNode:
                             print(f'エラー原因{e}')
                             #continue
                 print("これにてリーダー選挙を終了します")
+                time.sleep(2)
+                sys.exit()
             else:
                 print(f"Node {self.id} はリーダーがすでにいます")
                 return
@@ -171,8 +142,8 @@ if __name__ == "__main__":
 
     node_1 = BullyNode(1, 8001)
     node_2 = BullyNode(2, 8002)
-    node_3 = BullyNode(3, 8003, is_down=True)
-    node_4 = BullyNode(4, 8004)
+    node_3 = BullyNode(3, 8003)
+    node_4 = BullyNode(4, 8004, is_down=True)
     node_5 = BullyNode(5, 8005, is_down=True)
     
 
